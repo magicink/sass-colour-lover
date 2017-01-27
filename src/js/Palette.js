3c51afe4f8a2ae2d205da807a75409e3403261d6
Brandon Tom <magicink@gmail.com>
2017-01-26 16:09:51 -0800

    Re-organized the Palette model

--- src/js/Palette.js
+++ src/js/Palette.js
@@ -7,42 +7,42 @@ polyfill()
 export default {
   create (id = null) {
     let url = (id) ? `http://www.colourlovers.com/api/palette/${encodeURIComponent(id)}?format=json` : null
-    return Object.assign({
+    return Object.assign(Object.create(this), {
       author: null,
       colors: [],
       error: null,
-      get (success, failure) {
-        if (this.id && this.url) {
-          if (!failure) {
-            failure = (error) => {
-              console.log(error.toString())
-            }
-          }
-          if (!success) {
-            success = (data) => {
-              if (data.length === 1) {
-                data = data[0]
-                this.author = data.userName
-                this.title = data.title
-                let colors = data.colors
-              }
-            }
-          }
-          fetch(this.url).then((response) => {
-            if (response.status >= 400) {
-              this.error = {
-                status: response.status,
-                message: response.statusText
-              }
-              throw new Error(`${response.status} - ${response.statusText}\r\n${response.url}`)
-            }
-            return response.json()
-          }).then(success).catch(failure)
-        }
-      },
       id,
       title: null,
       url
     })
+  },
+  get (success, failure) {
+    if (this.id && this.url) {
+      if (!failure) {
+        failure = (error) => {
+          console.log(error.toString())
+        }
+      }
+      if (!success) {
+        success = (data) => {
+          if (data.length === 1) {
+            data = data[0]
+            this.author = data.userName
+            this.title = data.title
+            let colors = data.colors
+          }
+        }
+      }
+      fetch(this.url).then((response) => {
+        if (response.status >= 400) {
+          this.error = {
+            status: response.status,
+            message: response.statusText
+          }
+          throw new Error(`${response.status} - ${response.statusText}\r\n${response.url}`)
+        }
+        return response.json()
+      }).then(success).catch(failure)
+    }
   }
 }
